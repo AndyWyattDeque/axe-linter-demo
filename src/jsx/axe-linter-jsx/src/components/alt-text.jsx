d692862bd89c2b6aea63deecf20699893be0ac20
davidrmusser <david.musser@deque.com>
2023-06-15 11:28:12 -0400

    test

--- src/jsx/axe-linter-jsx/src/components/alt-text.jsx
+++ src/jsx/axe-linter-jsx/src/components/alt-text.jsx
@@ -2,24 +2,19 @@ import React from "react";
 
 const AltTextSucceed = () => (
   <div>
-    <img src="foo" alt="Foo eating a sandwich." />
     <img src="foo" alt={"Foo eating a sandwich."} />
     <img src="foo" alt={altText} />
     <img src="foo" alt={`${person} smiling`} />
-    <img src="foo" alt="" />
-
-    <object aria-label="foo" />
-    <object aria-labelledby="id1" />
-    <object>Meaningful description</object>
-    <object title="An object" />
-
+    <myImg src="foo" label={`${person} smiling`} />
     <area aria-label="foo" />
     <area aria-labelledby="id1" />
     <area alt="This is descriptive!" />
+    
+    <mybutton></mybutton>
+    <img src="foo" alt={"Foo eating a sandwich."} />
+    <myImg src="foo.jpg"/>
+    <myImg src="foo" label="foo"/>
 
-    <input type="image" alt="This is descriptive!" />
-    <input type="image" aria-label="foo" />
-    <input type="image" aria-labelledby="id1" />
   </div>
 );
 
